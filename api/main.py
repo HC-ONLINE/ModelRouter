b910d5e263313c42de3d214cc0a81173006d39f2
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-07 14:48:28 -0500

    fix: corregir la ruta de la aplicación en el comando de ejecución

--- api/main.py
+++ api/main.py
@@ -229,7 +229,7 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app.main:app",
+        "api.main:app",
         host=settings.host,
         port=settings.port,
         reload=settings.app_env == "development",
